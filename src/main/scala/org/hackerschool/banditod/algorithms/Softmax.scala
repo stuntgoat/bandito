94f7258753ce8e0e78c2985842b45f5b9cb37d3e
stungtoat <isaac@overttone.com>
2014-10-24 00:53:12 -0700

    select arm; update reward; softmax temp function

--- src/main/scala/org/hackerschool/banditod/algorithms/Softmax.scala
+++ src/main/scala/org/hackerschool/banditod/algorithms/Softmax.scala
@@ -1,35 +1,69 @@
 package org.hackerschool.banditod.algorithms
 
-import scala.util.Random
 
-
-case class Sofmax(
+case class Softmax(
   val softmaxTempNumerator: Double = 1.0,
 
-  /** Map arm names to tuple of (<ratio>, (<numerator>, <denominator>))
+  /**
+    Map arm names to tuple of (<ratio>, (<numerator>, <denominator>))
     for each arm's counts.
     */
   private var armRatioValues: Map[String, (Double, (Double, Double))] = Map()) extends Algorithm {
 
-  def softmaxTemperature(_counts: Double): Double = {
-    var counts: Double = 0
-    if (_counts > 0) {
-        counts = _counts
+  // Holds the total number of pulls to date.
+  private var totalDenominator: Double = 0
+
+  def softmaxTemperature(_count: Double): Double = {
+    /**
+      Calculate the temperature given '_count' argument.
+      */
+    var count: Double = 0
+    if (_count > 0) {
+        count = _count
     }
 
     /** Add 1 and a small fractional value
       to prevent divide by zero errors and values >= 1 */
-    this.softmaxTempNumerator / scala.math.log(counts + (1 + .1e-7))
+    this.softmaxTempNumerator / scala.math.log(count + (1 + .1e-7))
   }
 
+  def probsFromRatios(ratios: List[Double], temperature: Double): List[Double] = {
+    /**
+      Given an iterable of conversions and a temperature, generate
+      scaled and normalized probabilities for each.
+
+      Args:
+      - conversions: a list of conversion rates.
+      - temp: an 'annealing temperature'
+
+      */
+    val denominator = ratios.map(x => scala.math.exp(x / temperature)).sum
+    ratios.map(x => scala.math.exp(x / temperature) / denominator)
+  }
   /**
     Implement the Algorithm abstract class.
 
     */
 
   def selectArm(): String = {
-    /** TODO*/
-    ""
+    if (this.armRatioValues.size == 1) {
+      val arm: String = this.armRatioValues.keys.head
+      // Increment arm pull.
+      this.totalDenominator += 1
+      return arm
+    }
+
+    var arms: List[String] = List()
+    var ratios: List[Double] = List()
+    for ((arm, values) <- this.armRatioValues.iterator) {
+      arms ::= arm
+      ratios ::= values._1
+    }
+    val temperature: Double = this.softmaxTemperature(this.totalDenominator)
+    val probabilities: List[Double] = this.probsFromRatios(ratios, temperature)
+    val arm: String = ListUtils.categoricalChoice(probabilities, arms)
+    this.totalDenominator += 1
+    arm
   }
 
   def selectFromSubset(arms: List[String]): String = {
@@ -38,6 +72,10 @@ case class Sofmax(
   }
 
   def initialize(_names: List[String]) = {
+    /**
+      - Is this method needed since we initialize during the class instantiation?
+
+      */
     for (name <- _names) {
       this.addArm(name)
     }
@@ -58,6 +96,19 @@ case class Sofmax(
   }
   def updateReward(arm: String, reward: Double = 1): Boolean = {
     /** TODO: */
-    return false
+    val armValues = this.armRatioValues.get(arm)
+    if (!armValues.isEmpty) {
+      for ((ratio, (numerator, denominator)) <- armValues) {
+        val newNumerator = numerator + reward
+        if (denominator == 0) {
+          this.armRatioValues += arm -> (0, (newNumerator, denominator))
+        } else {
+          val newRatio = newNumerator / denominator
+          this.armRatioValues += arm -> (newRatio, (newNumerator, denominator))
+        }
+      }
+      return true
+    }
+    false
   }
 }
